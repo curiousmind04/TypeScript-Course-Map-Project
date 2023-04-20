e51ced0f494b404316caafc5feee53d80a42e328
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-04-20 13:49:17 -0400

    finished project

---
+++ src/app.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { Loader } from "@googlemaps/js-api-loader";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+// declare var google: any;
+
+const loader = new Loader({
+  apiKey: process.env.GOOGLE_API_KEY as string,
+  version: "weekly",
+});
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${process.env.GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      loader.load().then(() => {
+        const map = new google.maps.Map(
+          document.getElementById("map") as HTMLElement,
+          {
+            center: { lat: coordinates.lat, lng: coordinates.lng },
+            zoom: 16,
+          }
+        );
+        new google.maps.Marker({ position: coordinates, map: map });
+      });
+    })
+    //   const map = new google.maps.Map(
+    //     document.getElementById("map") as HTMLElement,
+    //     {
+    //       center: coordinates,
+    //       zoom: 16,
+    //     }
+    //   );
+
+    //   new google.maps.Marker({ position: coordinates, map: map });
+    // })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  devtool: "inline-source-map",
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new Dotenv()],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
